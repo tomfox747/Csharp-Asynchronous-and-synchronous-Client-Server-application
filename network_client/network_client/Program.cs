95f4907f597e2d49976a48c33986f7fa7019c9d0
Thomas Fox <47598118+tomfox747@users.noreply.github.com>
2019-08-25 15:37:28 +0100

    threading added

---
+++ network_client/network_client/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.NetworkInformation;
+
+namespace network_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //specify the ip address of the server and the port number
+            string serverIP = "192.168.0.44";
+            int portNumber = 7474;
+
+            //Allow the user to repeatedly send messages
+            while(true)
+            {
+                //ask user for message input
+                Console.WriteLine("input your message");
+                string Message = Console.ReadLine();
+
+                //Do not allow data formatting characters
+                if (Message.Contains('|'))
+                {
+                    Console.WriteLine("Illegal use of the '|' character");
+                }
+                else
+                {
+                    //attempt connection to the tcp client
+                    try
+                    {
+                        //create new tcp client object with the ip and port number of the server
+                        TcpClient client = new TcpClient(serverIP, portNumber);
+
+                        //break from program
+                        if (Message == "exit") { break; }
+
+                        //add message end character for data transfer
+                        Message += "|";
+
+                        //get the size of the message in bytes / create byte array for the message
+                        int byteCount = Encoding.ASCII.GetByteCount(Message);
+                        byte[] sendData = new byte[byteCount];
+
+                        //convert the message into a byte array
+                        sendData = Encoding.ASCII.GetBytes(Message);
+
+                        //create a data stream and write the message to that stream / add timeout for the stream reader
+                        NetworkStream stream = client.GetStream();
+                        stream.ReadTimeout = 500;
+                        stream.Write(sendData, 0, sendData.Length);
+
+                        //create a byte array to store returned inforamtion from the server
+                        byte[] returnedInfo = new byte[1000];
+
+                        //checks for response for server using readtimeout
+                        try
+                        {
+                            //read data from the data stream
+                            stream.Read(returnedInfo, 0, returnedInfo.Length);
+
+                            //send the data to the function to remove empty bytes
+                            byte[] formattedInfo = removeEmptyBytes(returnedInfo);
+
+                            //convert data into a string
+                            string returnedMessage = Encoding.ASCII.GetString(formattedInfo, 0, formattedInfo.Length);
+                            Console.WriteLine(returnedMessage);
+                        }catch(Exception e)
+                        {
+                            Console.WriteLine("no response from the server - message may have not sent");
+                        }
+
+                        //close the stream and the client
+                        stream.Close();
+                        client.Close();
+                    }
+                    catch (Exception e)
+                    { 
+                        Console.WriteLine(e.ToString());
+                    }
+                }
+            }
+        }
+
+        public static byte[] removeEmptyBytes(byte[] input)
+        {
+            string inputString = Encoding.ASCII.GetString(input);
+            string outputString = "";
+            foreach(char c in inputString)
+            {
+                if(c == '|')
+                {
+                    break;
+                }
+                else
+                {
+                    outputString += c;
+                }
+            }
+            
+            byte[] outputArray = new byte[outputString.Length];
+            outputArray = Encoding.ASCII.GetBytes(outputString);
+            return outputArray;
+        }
+    }
+}
---
+++ network_server/network_server/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace network_server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Specify the ip address and the port of the server - If running remotely, listen on 0.0.0.0. Port number doesn't matter
+            string ipAddress = "127.0.0.1";
+            int portNumber = 7474;
+
+            //set the ip address / create the server and client objects
+            IPAddress ip = IPAddress.Parse(ipAddress);
+            TcpListener server = new TcpListener(ip, portNumber);
+            TcpClient client = default(TcpClient);
+
+            //attempts to connect to server with a try catch
+            try
+            {
+                //start the server
+                server.Start();
+                Console.WriteLine("server is running on port : {0}", portNumber);
+
+                //listening loop starts
+                while(true)
+                {
+                    //create a byte array to hold information from the client - create string to hold ASCII converted message
+                    byte[] recievedBuffer = new byte[1000];
+                    string recievedMessage;
+
+                    //save the client to the server / create a data stream
+                    client = server.AcceptTcpClient();
+                    NetworkStream stream = client.GetStream();
+                    
+                    //read data to stream / pass parameters... byte array, position to begin reading, byte array size
+                    stream.Read(recievedBuffer, 0, recievedBuffer.Length);
+                    
+                    //Remove all null bytes in the buffer and save to new array
+                    byte[] formattedArray = removeEmptyBytesFromArray(recievedBuffer);
+                    //Convert the formatted array to a string
+                    recievedMessage = byteArrayToString(formattedArray);
+                    //Remove any data transfer formatting from the string
+                    recievedMessage = deFormatString(recievedMessage);
+                    
+                    //Print the information to the console window
+                    Console.WriteLine("-- " + recievedMessage + " --");
+
+                    //Return information to the client machine and console / create byte array response to send
+                    byte[] response = formatResponse();
+                    respondToRequest(stream, response);
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        //convert any byte array to a string
+        public static string byteArrayToString(byte[] input)
+        {
+            string output = Encoding.ASCII.GetString(input, 0, input.Length);
+            return output;
+        }
+        //Convert any string to a byte array
+        public static byte[] stringToByteArray(string input)
+        {
+            byte[] outputArray = new byte[Encoding.ASCII.GetByteCount(input)];
+            outputArray = Encoding.ASCII.GetBytes(input);
+            return outputArray;
+        }
+
+        //Add any string formatting required for the data transfer
+        public static string formatString(string input)
+        {
+            input += '|';
+            return input;
+        }
+        //Remove any formatting used for the data transfer
+        public static string deFormatString(string input)
+        {
+            string output = "";
+
+            foreach(char c in input)
+            {
+                if(c == '|')
+                {
+                    break;
+                }
+                else
+                {
+                    output += c;
+                }
+            }
+
+            return output;
+        }
+
+        //create a response to the client request
+        public static byte[] formatResponse()
+        {
+            byte[] confirmationByteArray = new byte[1000];
+            string confirmationMessage = "your message has been recieved";
+            confirmationMessage += '|';
+            confirmationByteArray = stringToByteArray(confirmationMessage);
+            confirmationByteArray = removeEmptyBytesFromArray(confirmationByteArray);
+
+            return confirmationByteArray;
+        }
+
+        //Write the response to the data stream
+        public static void respondToRequest(NetworkStream stream, byte[] data)
+        {
+            stream.Write(data, 0, data.Length);
+        }
+        
+        //Remove any empty bytes from a byte array
+        public static byte[] removeEmptyBytesFromArray(byte[] input)
+        {
+            string fullString = byteArrayToString(input);
+            string outputString = "";
+            foreach(char c in fullString)
+            {
+                if (c == '|')
+                {
+                    outputString += '|';
+                    break;
+                }
+                else
+                {
+                    outputString += c;
+                }
+            }
+
+            byte[] messageArray = new byte[outputString.Length];
+            messageArray = Encoding.ASCII.GetBytes(outputString);
+
+            return messageArray;
+        }
+    }
+}
